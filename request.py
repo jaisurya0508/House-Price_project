0680857ed3fbfadfe45a690f1d265dd4083a851e
jaisurya0508 <67913156+jaisurya0508@users.noreply.github.com>
2020-07-06 20:20:51 +0530

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://localhost:5000/predict-api' 
+url = 'http://localhost:5000/predict_api' 
 r = requests.post(url,json={'area':3000, 'bedrooms':2, 'age':6})
 
 print(r.json())
